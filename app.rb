f127637e72ee9924cd7fcf8cd0a793d330ff1b2d
Jimmy Chen <jimmyjchen@outlook.com>
2019-07-01 16:18:09 +0800

    Completed Cookbook web app

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ require_relative 'cookbook'
 require_relative 'recipe'
 require_relative 'search_recipes'
 
+# Create global variables for csv_file and cookbook to have DRY code
+
+set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
@@ -22,13 +26,33 @@ get '/new' do
   erb :new
 end
 
-post '/recipes' do
+post '/import' do
+  @recipes = SearchRecipes.call(params[:ingredient])
+  erb :import
+end
+
+post '/download' do
   @csv_file = File.join(__dir__, 'recipes.csv')
   @cookbook = Cookbook.new(@csv_file)
   recipe = Recipe.new(params[:name], params[:description], params[:prep_time], false, params[:difficulty])
   @cookbook.add_recipe(recipe)
   @cookbook_array = @cookbook.all
-  erb :index
+  redirect to('/')
 end
 
+post '/recipes' do
+  @csv_file = File.join(__dir__, 'recipes.csv')
+  @cookbook = Cookbook.new(@csv_file)
+  recipe = Recipe.new(params[:name], params[:description], params[:prep_time] + " min", false, params[:difficulty])
+  @cookbook.add_recipe(recipe)
+  @cookbook_array = @cookbook.all
+  redirect to('/')
+end
 
+post '/delete' do
+  @csv_file = File.join(__dir__, 'recipes.csv')
+  @cookbook = Cookbook.new(@csv_file)
+  @cookbook.remove_recipe(params[:index].to_i)
+  @cookbook_array = @cookbook.all
+  redirect to('/')
+end
